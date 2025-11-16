f53e3217baa682bcd05e1f025afc35d6c4dc900c
Bowles.Bradley@mayo.edu <bbowles1@alumni.nd.edu>
2025-11-16 08:29:32 -0600

    add json export script

--- src/gtf_to_db/json_converter.py
+++ src/gtf_to_db/json_converter.py
@@ -15,18 +15,17 @@ class NpEncoder(json.JSONEncoder):
             return obj.tolist()
         return super(NpEncoder, self).default(obj)
 
-def export_uorfs(uorfs):
+def export_uorfs(uorfs, outpath):
     """Write the output uorf JSON
 
     :param uorfs: JSON structure containing uORF data
     :type uorfs: JSON
+    :param outpath: full export path, including JSON extension
+    :type outpath: str
     """
-    # Ensure the directory exists
-    os.makedirs('./data', exist_ok=True)
     
     # Write to JSON file
-    with open('./data/uorfs.json', 'w') as f:
-        #json.dump(uorfs, f)
+    with open(outpath, 'w') as f:
         json.dump(uorfs, f, cls=NpEncoder)
     
 
@@ -44,6 +43,95 @@ def query_uorf_db(database_path, table, transcript):
     except sqlite3.Error as e:
         print(f"Database error: {e}")
         return []
+
+
+def format_utr(utr_tuple):
+    
+    out_json = {
+        "exon": utr_tuple.exon,
+        "type": "utr",
+        "start": utr_tuple.rel_start,
+        "end": utr_tuple.rel_stop
+        }
+    return out_json
+
+
+def format_cds(cds_tuple, cds_start):
+    out_json = {
+        "exon": cds_tuple.exon,
+        "type": "cds",
+        "start": cds_start,
+        "end": cds_start + cds_tuple.length
+        }
+    return out_json
+
+def format_uorfs(uorf_tuple):
+    uorf_start = uorf_tuple.rel_start_pos
+    uorf_stop = uorf_tuple.rel_stop_pos
+    out_json = {
+        "id": uorf_tuple.uorf_id,
+        "type": "uorf",
+        "frame": uorf_tuple.frame,
+        "start": uorf_start,
+        "end": uorf_stop,
+        "exons": [
+            {
+                "exon": "1",
+                "type": "uorf",
+                "start_codon": uorf_tuple.start_codon,
+                "start": uorf_start,
+                "end": uorf_stop
+            }
+        ],
+        "start_codon": uorf_tuple.start_codon,
+        "stop_codon": uorf_tuple.stop_codon,
+        "length": uorf_tuple.uorf_length,
+        "sequence": uorf_tuple.uORF_FASTA
+        }
+    return out_json
+
+
+def assemble_json_from_transcript(database_path, target_transcript, outpath):
+
+    enst_query = query_uorf_db(database_path, "transcripts", target_transcript) # transcript
+    utr_query = query_uorf_db(database_path, "utr", target_transcript) # utr
+    cds_query = query_uorf_db(database_path, "cds", target_transcript) # cds
+    uorf_query = query_uorf_db(database_path, "uorfs", target_transcript) # cds
+    
+    # check for multiple transcript returns
+    if enst_query.shape[0] != 1:
+        raise Exception("Multiple transcripts returned!")
+    enst = enst_query.iloc[0]
+        
+    transcript_exons = [format_utr(row) for row in utr_query.itertuples()] + [
+        format_cds(cds_query.iloc[0], utr_query.rel_stop.iloc[-1])]
+        
+    # format transcript block of JSON (transcript meta + exons)
+    transcript_block = {
+        "id": enst.transcript,
+        "type": "transcript",
+        "start": 0,
+        "end": enst.length,
+        "exons": transcript_exons
+        }
+
+    # format uORF blocks    
+    uorf_block = [format_uorfs(row) for row in uorf_query.itertuples()]
+    
+    uorf_json = {
+        "gene":enst.gene_id,
+        "start":0,
+        "end":enst.length,
+        "regions": [
+            [transcript_block] + uorf_block
+            ]
+        }
+
+    # print first element of uORF block for debugging
+    print(json.dumps(uorf_block[0], indent=4))
+
+    # save to outpath
+    export_uorfs(uorf_json, outpath)
 
 
 __all__ = ['NpEncoder','export_uorfs','query_uorf_db']
